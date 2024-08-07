6791c30ccbe5d7e6f46048617bb4846f8bf7ce57
Nuttapong <kantika@Kantikas-Air.lan>
2024-08-08 00:32:51 +1000

    comment crashlytic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.storage)
-    implementation(libs.firebase.crashlytics)
+//    implementation(libs.firebase.crashlytics)
     implementation(libs.timber)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.glide)
