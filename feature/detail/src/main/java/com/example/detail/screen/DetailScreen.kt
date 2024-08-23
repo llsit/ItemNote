7041e13adada5ef6f6bc4bb5f67297784ed3326a
Nuttapong <kantika@Kantikas-Air.lan>
2024-08-24 01:28:20 +1000

    add read more

--- feature/detail/src/main/java/com/example/detail/screen/DetailScreen.kt
+++ feature/detail/src/main/java/com/example/detail/screen/DetailScreen.kt
@@ -1,7 +1,9 @@
 package com.example.detail.screen
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -32,12 +34,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -96,6 +102,9 @@ fun RecipeDetailContent(
     padding: PaddingValues,
     onVideoClicked: (String) -> Unit
 ) {
+    var isExpanded by remember { mutableStateOf(false) }
+    var needsToBeExpandable by remember { mutableStateOf(false) }
+    val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
     Box(
         modifier = Modifier
             .padding(padding)
@@ -162,11 +171,29 @@ fun RecipeDetailContent(
             Text(
                 recipeInfo.instructions,
                 modifier = Modifier
-                    .fillMaxWidth(),
-                maxLines = 3,
+                    .fillMaxWidth()
+                    .animateContentSize(),
+                maxLines = if (isExpanded) Int.MAX_VALUE else 3,
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.Gray
+                color = Color.Gray,
+                onTextLayout = { textLayoutResult ->
+                    if (!isExpanded && textLayoutResult.hasVisualOverflow) {
+                        needsToBeExpandable = true
+                    }
+                    textLayoutResultState.value = textLayoutResult
+                },
             )
+            if (needsToBeExpandable) {
+                Text(
+                    text = if (isExpanded) "Read less" else "Read more",
+                    style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary),
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .clickable {
+                            isExpanded = !isExpanded
+                        }
+                )
+            }
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 "Ingredients",
