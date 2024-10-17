4e08303324e004aa90165c16f4894af67d980b62
Nuttapong <kantika@Kantikas-Air.lan>
2024-10-18 01:27:46 +1100

    - fix version - add search and favorite screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.0.8"
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
